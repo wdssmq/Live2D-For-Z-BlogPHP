41b9cc8f93ab5214f9aeaa795ed663babdcfdb90
wdssmq <wdssmq@qq.com>
2023-06-24 08:40:47 +0800

    pub. 发布更新

--- about.php
+++ about.php
@@ -1,61 +1,203 @@
-<!--
-  <div class="SubMenu">
-    <a href="main.php" title="首页"><span class="m-left m-now">首页</span></a>
-    <?php require_once "about.php"; ?>
-  </div>
--->
-
 <style type="text/css">
-#divMain .SubMenu {height: auto;}
-i.xnxf-tip {
-	background-color: red;
-	border-radius: 7px;
-	height: 7px;
-	padding: 0;
-	position: relative;
-	right: 8px;
-	top: -3px;
-	width: 7px;
-}
-#help a {
+  .relative {
+    position: relative;
+  }
+
+  .clearfix::after {
+    display: block;
+    content: "";
+    clear: both;
+  }
+
+  .is-text-bold {
+    font-weight: bold;
+  }
+
+  .is-text-red {
+    color: #d60000;
+  }
+
+  #divMain2 {
+    margin-bottom: 3rem;
+  }
+
+  #divMain2 p:not(:last-child) {
+    margin-bottom: .3rem;
+  }
+
+  #divMain2 code {
+    background-color: hsl(0deg, 0%, 96%);
+    color: hsl(348deg, 100%, 61%);
+    font-size: 0.875em;
+    font-weight: normal;
+    padding: 0.25em 0.5em 0.25em;
+  }
+
+  #divMain .SubMenu {
+    height: auto;
+  }
+
+  .tab-content form,
+  .tab-content table {
+    margin-top: 0;
+  }
+
+  .tab-content .xnxf-about-btn {
+    display: none;
+  }
+
+  .xnxf-fade>*:not(.content-box) {
+    opacity: 0;
+  }
+
+  .xnxf-fade>.in {
+    opacity: 1;
+  }
+
+  i.xnxf-tip-dot {
+    background-color: red;
+    border-radius: 50%;
+    position: relative;
+    top: -3px;
+    right: -3px;
+    height: 7px;
+    width: 7px;
+    margin-left: -7px;
+    padding: 0;
+  }
+
+  #xnxf-about {
+    padding-top: 1em;
+    display: none;
+    clear: both;
+  }
+
+  #xnxf-contrib {
+    display: none;
+  }
+
+  #xnxf-contrib.absolute {
+    position: absolute;
+    display: block;
+    background-color: #fff;
+    box-shadow: 0 0 0.1em rgb(0 0 0 / 30%);
+    left: 0;
+    top: 0;
+    right: 0;
+  }
+
+  #xnxf-contrib>* {
+    width: 960px;
+    margin: .7rem auto;
+  }
+
+  #xnxf-qr-box {
+    background-color: #DDDDDD;
+    padding: 11px 0;
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  #xnxf-about a {
     background-color: inherit;
     float: none;
-}
-#help td {
-    padding: 7px;
-    font-size: 15px;
-}
-</style>
-<i class="xnxf-tip" style="float: right"></i>
-<a href="javascript:;" style="float: right;" onclick="$('#help,#divMain2').slideToggle();$('.xnxf-tip').animate({opacity: 0});SetCookie('xnxf-tip', 'xnxf', 365)" title="查看/隐藏关于"><span class="m-right">[关于]</span></a>
-<div id="help" style="display:none;clear: both">
-<table>
-<tr>
-  <td>QQ：</td>
-  <td><a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=349467624&site=qq&menu=yes"><img src="//wpa.qq.com/pa?p=2:349467624:51" alt="置百丈玄冰而崩裂，掷须臾池水而漂摇。" title="置百丈玄冰而崩裂，掷须臾池水而漂摇。"/></a></td>
-  <td>349467624</td>
-</tr>
-<tr>
-  <td>Q群：</td>
-  <td><a target="_blank" href="//shang.qq.com/wpa/qunwpa?idkey=7a69c73adf802624837fcad9e9294a82263b94269408930501b34247ee2cbd2c"><img src="//pub.idqqimg.com/wpa/images/group.png" alt="我的咸鱼心" title="我的咸鱼心"></a></td>
-  <td>189574683</td>
-</tr>
-<tr>
-  <td>更多应用：</td>
-  <td><a href="https://app.zblogcn.com/?auth=6401c4a7-89cd-48f9-a68b-d6464d8c3bc8" title="Z-Blog 应用中心-沉冰浮水" target="_blank" rel="noopener noreferrer">Z-Blog 应用中心-沉冰浮水</a></td>
-  <td></td>
-</tr>
-<tr>
-	<td colspan="3" style="text-align: center">置百丈玄冰而崩裂，掷须臾池水而漂摇。</td>
-</tr>
+  }
 
-</table>
-<p>－－－－下边是推广链接－－－－</p>
-<!-- <> -->
+  hr {
+    background-color: navajowhite;
+    border: none;
+    display: block;
+    height: 1px;
+    margin: 1rem 0;
+    visibility: visible;
+  }
+</style>
+<a class="xnxf-about-btn" href="javascript:;" style="float: right;" title="查看/隐藏关于">
+  <i class="xnxf-tip-dot" style="float: right"></i>
+  <span class="m-right">[关 于]</span>
+</a>
+<div id="xnxf-about">
+  <!-- -------- -->
+  <p>QQ：349467624 <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=349467624&amp;site=qq&amp;menu=yes" title="沉冰浮水的QQ"><img src="https://www.wdssmq.com/zb_users/logos/qq_button.png" alt="QQ" title="QQ"></a></p>
+  <p>博客：<a target="_blank" href="https://www.wdssmq.com" title="沉冰浮水">https://www.wdssmq.com</a></p>
+  <p class="is-text-bold">Feed：<a target="_blank" href="https://feed.wdssmq.com" title="Feed">https://feed.wdssmq.com</a></p>
+  <p>bilibili：<a target="_blank" href="https://space.bilibili.com/44744006" title="bilibili">https://space.bilibili.com/44744006</a></p>
+  <p>GitHub：<a target="_blank" href="https://github.com/wdssmq" title="GitHub">https://github.com/wdssmq</a></p>
+  <p>GreasyFork：<a target="_blank" href="https://greasyfork.org/zh-CN/users/6865-wdssmq" title="GreasyFork">https://greasyfork.org/zh-CN/users/6865-wdssmq</a></p>
+  <p>嘟特（Mastodon ）：<a href="https://wxw.moe/@wdssmq" target="_blank" title="沉冰浮水 (@wdssmq@wxw.moe) - 呜呜 w(> ʌ <)w">https://wxw.moe/@wdssmq</a></p>
+  <p>知乎：<a target="_blank" href="https://www.zhihu.com/people/wdssmq" title="知乎">https://www.zhihu.com/people/wdssmq</a></p>
+  <hr><!-- -------- -->
+  <p>文章推荐：「<a target="_blank" href="https://www.wdssmq.com/post/20201231613.html" title="「言说」RSS 是一种态度！！">RSS 是一种态度</a>」「<a href="https://www.wdssmq.com/post/20200617652.html" target="_blank" title="「水坑」系列教程索引_电脑网络_沉冰浮水">「水坑」系列教程索引_电脑网络_沉冰浮水</a>」</p>
+  <hr><!-- -------- -->
+  <p>爱发电：<a target="_blank" href="https://afdian.net/@wdssmq" title="爱发电">https://afdian.net/@wdssmq</a></p>
+  <p>[AD：<a title="老薛主机" target="_blank" href="https://my.laoxuehost.net/aff.php?aff=294">PHP 美国空间</a> 优惠码：15off-xnxf ]</p>
+  <p>[AD：<a title="主机云" target="_blank" href="https://my.hostyun.com/page.aspx?c=referral&u=8680">vps-主机云</a>]</p>
+  <p>更多广告：<a title="Vultr" target="_blank" href="https://www.wdssmq.com/category/%E5%B9%BF%E5%91%8A%E6%85%8E%E5%85%A5/">广告慎入 - 沉冰浮水</a></p>
+  <p>更多应用：<a href="https://app.zblogcn.com/?auth=6401c4a7-89cd-48f9-a68b-d6464d8c3bc8" target="_blank" title="沉冰浮水 - Z-Blog 应用中心">沉冰浮水 - Z-Blog 应用中心</a></p>
+  <hr><!-- -------- -->
+  <p>一款基于 QueryList V4 的 Z-Blog 采集插件：<a href="https://github.com/wdssmq/kumo-for-zblog" target="_blank" title="wdssmq/kumo-for-zblog: 一款基于QueryList V4的Z-Blog采集插件">https://github.com/wdssmq/kumo-for-zblog</a></p>
+  <p>某理念崩坏的采集插件 - Z-Blog 应用中心：<a href="https://app.zblogcn.com/?id=17788" target="_blank" title="某理念崩坏的采集插件 - Z-Blog 应用中心">https://app.zblogcn.com/?id=17788</a></p>
+  <div style="min-height: 37px;"></div>
 </div>
-<div style="clear: both"></div>
+<div id="xnxf-contrib" class="in">
+  <p>虽然暂时还没有做区免费 Or 收费的判断，假定您是免费获取到的，而这个插件对您有用，请支付合理费用。</p>
+  <p><b>点击上方「关于」可关闭此提示，137 天内不再显示。</b></p>
+  <div id="xnxf-qr-box">
+    <img src="https://fastly.jsdelivr.net/gh/wdssmq/wdssmq@main/doc/qr-qq.png" alt="qr-qq" title="qr-qq" style="max-width:100%;">
+    <img src="https://fastly.jsdelivr.net/gh/wdssmq/wdssmq@main/doc/qr-wx.png" alt="qr-wx" title="qr-wx" style="max-width:100%;">
+    <img src="https://fastly.jsdelivr.net/gh/wdssmq/wdssmq@main/doc/qr-ali.png" alt="qr-ali" title="qr-ali" style="max-width:100%;">
+  </div>
+  <div style="min-height: 37px;"></div>
+</div>
+<div class="clearfix"></div>
 <script type="text/javascript">
-if(GetCookie("xnxf-tip")=="xnxf"){
-  $('.xnxf-tip').css({opacity: 0});
-}
+  $(function() {
+    const mz_blogtitle = '<?php echo $blogtitle; ?>';
+    const mz_cookie = GetCookie("xnxf-about-tip") || "";
+
+    let mz_ckeSet = 0;
+
+    if (mz_cookie.indexOf(mz_blogtitle) > -1 && 1 == 1) {
+      mz_ckeSet = 1;
+      fnHideTipDot();
+    } else {
+      $("#divMain2,.content-box-content").addClass("relative xnxf-fade");
+      let $elWrap;
+      if ($(".content-box-content").length > 0) {
+        $elWrap = $(".content-box-content");
+      } else {
+        $elWrap = $("#divMain2");
+      }
+      $("#xnxf-contrib").appendTo($elWrap).addClass("absolute");
+    }
+
+    function fnHideTipDot() {
+      $('.xnxf-tip-dot').css({
+        opacity: 0
+      });
+    }
+
+    function fnAboutToggle(inTab = false) {
+      $(".absolute").removeClass("absolute");
+      $(".xnxf-fade").removeClass("xnxf-fade");
+      if (inTab) {
+        $("#xnxf-about,#xnxf-contrib").slideDown();
+      } else {
+        fnHideTipDot();
+        $("#divMain2,#xnxf-about,#xnxf-contrib").slideToggle();
+      }
+      if (mz_ckeSet == 0) {
+        SetCookie('xnxf-about-tip', `${mz_cookie},${mz_blogtitle}`, 137)
+      }
+    }
+
+    $(".xnxf-about-btn").click(function() {
+      if ($(this).hasClass("tab-btn")) {
+        fnAboutToggle(1);
+      } else {
+        fnAboutToggle();
+      }
+    });
+  });
 </script>
